dcea663bd8e25db90ce8aef99f2445ae91ed5cfc
LRyan <lwryan6@gmail.com>
2022-10-08 02:26:00 -0500

    Completed List Views: Display List of Jobs (job-list.html) & Add View All Link (list.html & ListController.java)

--- src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/ListController.java
@@ -29,36 +29,46 @@ public class ListController {
         columnChoices.put("positionType", "Position Type");
         columnChoices.put("coreCompetency", "Skill");
 
+//   Task 2, Part 2: Add 'View All' Link to the 'All' column on the /list 'View Jobs By Category' page from list.html file.
+//  Added a key/value pair to tableChoices in ListController() below by using 'all' for key name, (from listJobsByColumnAndValue() method further below.)
+        tableChoices.put("all","View All Jobs");
+
         tableChoices.put("employer", JobData.getAllEmployers());
         tableChoices.put("location", JobData.getAllLocations());
         tableChoices.put("positionType", JobData.getAllPositionTypes());
         tableChoices.put("coreCompetency", JobData.getAllCoreCompetency());
+
     }
 
     @GetMapping(value = "")
     public String list(Model model) {
         model.addAttribute("columns", columnChoices);
-        model.addAttribute("tableChoices", tableChoices);
+        model.addAttribute("tableChoices", tableChoices);   //modified tableChoices by adding k/v pair in constructor above.
         model.addAttribute("employers", JobData.getAllEmployers());
         model.addAttribute("locations", JobData.getAllLocations());
         model.addAttribute("positions", JobData.getAllPositionTypes());
         model.addAttribute("skills", JobData.getAllCoreCompetency());
 
-        return "list";
+        return "list";      // routes to list.html
     }
 
     @GetMapping(value = "jobs")
     public String listJobsByColumnAndValue(Model model, @RequestParam String column, @RequestParam(required = false) String value) {
         ArrayList<Job> jobs;
+
+//   Task 2, Part 2: Add 'View All' Link to the 'All' column on the /list 'View Jobs By Category' page from list.html file.
+//  Used 'all' from column.equals('all') below for the key when I added a key/value pair to tableChoices in ListController() above.
         if (column.equals("all")){
+
             jobs = JobData.findAll();
             model.addAttribute("title", "All Jobs");
         } else {
             jobs = JobData.findByColumnAndValue(column, value);
             model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
         }
+//  Task 2, Part 1: Display List of Jobs - This is where I got the attribute name 'jobs' to loop through in list-jobs.html
         model.addAttribute("jobs", jobs);
 
-        return "list-jobs";
+        return "list-jobs";     // routes to list-jobs.html
     }
 }
